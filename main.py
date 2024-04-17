f49f1773e4b789132084c6558ac3dfccdc2f409c
yuriwa <yuri.warczynski@gmail.com>
2024-04-17 03:54:25 +0200

    finalized adding of all parameters. bug fixes

--- main.py
+++ main.py
@@ -27,9 +27,10 @@ from rich.progress      import Progress
 from rich.table         import Table
 from rich.console       import Console
 
-import  argparse
+import argparse
 import signal
 import sys
+import pandas as pd
 
 # Define a function to handle termination signals
 def signal_handler(sig, frame):
@@ -41,31 +42,94 @@ signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
 # Helper function to create agents
-def create_agents_from_df(row, progress, agent_task, llm_task):
-    model_name = row.get('Model Name', 'gpt-4-turbo-preview').strip()
-    temperature = float(row.get('Temperature', 0.7))
-    num_ctx = int(row.get('Context size', 2048))
-
-    llm = get_llm(model_name, temperature, progress, llm_task, num_ctx=num_ctx)
-    if llm is None:
-        raise ValueError(f"Failed to retrieve or initialize the language model for {model_name}")
-
+def create_agents_from_df(row, 
+                          models_df = None, 
+                          progress  = None,  #rich pb
+                          agent_task= None,  #rich pb
+                          llm_task  = None): #rich pb
     def get_agent_tools(tools_string):
         tool_names = [tool.strip() for tool in tools_string.split(',')]
         return [getattr(ToolsMapping, tool) for tool in tool_names if hasattr(ToolsMapping, tool)]
+    
+    role             = row['Agent Role']
+    goal             = dedent(row['Goal'])
+    backstory        = dedent(row['Backstory'])
+    allow_delegation = helpers.str_to_bool(row['Allow delegation'])
+    verbose          = helpers.str_to_bool(row['Verbose'])
+    tools            = get_agent_tools(row['Tools'])
+    memory           = helpers.str_to_bool(row['Memory'])
+    max_iter         = row['Max_iter']
+    
+    #region LLMs
+    model_name       = row.get('Model Name', 'gpt-4-turbo-preview').strip()
+    temperature      = float(row.get('Temperature', 0.7))
+    
+    # Retrieve Agent model details
+    model_details = models_df[models_df['Model'].str.strip() == model_name] # Filter the models dataframe for the specific model
 
+    if model_details.empty:
+        llm = None
+        raise ValueError(f"Failed to retrieve or initialize the language model for {model_name}")
+    else:
+        # Retrieve each attribute, ensuring it exists and is not NaN; otherwise, default to None
+        num_ctx     = int(model_details['Context size (local only)'].iloc[0]) if 'Context size (local only)' in model_details.columns and not pd.isna(model_details['Context size (local only)'].iloc[0]) else None
+        provider    = str(model_details['Provider'].iloc[0]) if 'Provider' in model_details.columns and not pd.isna(model_details['Provider'].iloc[0]) else None
+        base_url    = str(model_details['base_url'].iloc[0]) if 'base_url' in model_details.columns and not pd.isna(model_details['base_url'].iloc[0]) else None
+        deployment  = str(model_details['Deployment'].iloc[0]) if 'Deployment' in model_details.columns and not pd.isna(model_details['Deployment'].iloc[0]) else None
+        
+        llm = get_llm(
+                    model_name  = model_name, 
+                    temperature = temperature, 
+                    num_ctx     = num_ctx, 
+                    provider    = provider,
+                    base_url    = base_url,
+                    deployment  = deployment, 
+                    progress    = progress, 
+                    llm_task    = llm_task
+                    
+                )
+    
+    # Retrieve function calling model details
+    function_calling_model_name = row.get('Function Calling Model', model_name)
+    if isinstance(function_calling_model_name, str):
+        #print(function_calling_model_name)
+        function_calling_model_name = function_calling_model_name.strip() 
+        function_calling_model_details = models_df[models_df['Model'].str.strip() == function_calling_model_name]
+    else:
+        function_calling_model_details = None
+
+    if function_calling_model_details is None or function_calling_model_details.empty:
+        function_calling_llm = llm
+    else:
+        num_ctx=int(function_calling_model_details['Context size (local only)'].iloc[0]) if 'Context size (local only)' in function_calling_model_details.columns and not pd.isna(function_calling_model_details['Context size (local only)'].iloc[0]) else None
+        provider=function_calling_model_details['Provider'].iloc[0] if 'Provider' in function_calling_model_details.columns and not pd.isna(function_calling_model_details['Provider'].iloc[0]) else None
+        base_url=function_calling_model_details['base_url'].iloc[0] if 'base_url' in function_calling_model_details.columns and not pd.isna(function_calling_model_details['base_url'].iloc[0]) else None
+        deployment=function_calling_model_details['Deployment'].iloc[0] if 'Deployment' in function_calling_model_details.columns and not pd.isna(function_calling_model_details['Deployment'].iloc[0]) else None
+ 
+        function_calling_llm = get_llm(
+                    model_name  = function_calling_model_name,
+                    temperature = temperature,  
+                    num_ctx     = num_ctx,
+                    provider    = provider,
+                    base_url     = base_url,
+                    deployment  = deployment,
+                    progress    = progress,
+                    llm_task     = llm_task
+                )
+    #endregion
+    
     agent_config = {
         #agent_executor:                                            #An instance of the CrewAgentExecutor class.
-        'role'            : row['Agent Role'],
-        'goal'            : dedent(row['Goal']),
-        'backstory'       : dedent(row['Backstory']),
-        'allow_delegation': helpers.str_to_bool(row['Allow delegation']),   #Whether the agent is allowed to delegate tasks to other agents.
-        'verbose'         : helpers.str_to_bool(row['Verbose']),            #Whether the agent execution should be in verbose mode.
-        'tools'           : get_agent_tools(row['Tools']),          #Tools at agents disposal
-        'memory'          : True,                                   #TODO: Remove hardcoding #Whether the agent should have memory or not.
-        'max_iter'        : 2000,                                   #TODO: Remove hardcoding #Maximum number of iterations for an agent to execute a task.
+        'role'            : role,
+        'goal'            : goal,
+        'backstory'       : backstory,
+        'allow_delegation': allow_delegation,   #Whether the agent is allowed to delegate tasks to other agents.
+        'verbose'         : verbose,            #Whether the agent execution should be in verbose mode.
+        'tools'           : tools,          #Tools at agents disposal
+        'memory'          : memory,     #Whether the agent should have memory or not.
+        'max_iter'        : max_iter,                                   #TODO: Remove hardcoding #Maximum number of iterations for an agent to execute a task.
         'llm'             : llm,                                    #The language model that will run the agent.
-        'function_calling_llm': llm                                 #The language model that will the tool calling for this agent, it overrides the crew function_calling_llm.
+        'function_calling_llm': function_calling_llm                                #The language model that will the tool calling for this agent, it overrides the crew function_calling_llm.
         #step_callback:                                             #Callback to be executed after each step of the agent execution.
         #callbacks:                                                 #A list of callback functions from the langchain library that are triggered during the agent's execution process
     }                                                               
@@ -85,19 +149,41 @@ def create_tasks_from_df(row, assignment, created_agents):
         agent               = get_agent_by_role(created_agents, desired_role)
     )
 
-def create_crew(created_agents, created_tasks):
+def create_crew(created_agents, created_tasks, crew_df):
+    embedding_model = crew_df['Embedding model'][0]
+    deployment_name = models_df.loc[models_df['Model'] == embedding_model, 'Deployment'].values[0]
+    provider = models_df.loc[models_df['Model'] == embedding_model, 'Provider'].values[0]
+    base_url = models_df.loc[models_df['Model'] == embedding_model, 'base_url'].values[0]
+    verbose = helpers.str_to_bool(crew_df['Verbose'][0])
+    process = Process.hierarchical if crew_df['Process'][0] == 'hierarchical' else Process.sequential
+    memory = helpers.str_to_bool(crew_df['Memory'][0])
+    
+    config = {"model": embedding_model,}
+    
+    #Create provide specific congig and load proveder specific ENV variables if it can't be avoided
+    if provider == 'azure-openai':
+        config['deployment_name'] = deployment_name             #Set azure specific config
+        #os.environ["AZURE_OPENAI_DEPLOYMENT"] = deployment_name #Wrokarond since azure 
+        os.environ["OPENAI_API_KEY"] = os.environ["AZURE_OPENAI_KEY"]
+
+    if provider == 'openai':
+        config['api_key'] = os.environ.get("OPENAI_API_KEY")
+        os.environ["OPENAI_BASE_URL"] = "https://api.openai.com/v1"
+    else: #Any other openai compatible e.g. ollama or llama-cpp				
+        provider = 'openai'
+        api_key = 'NA'
+        config['base_url'] = base_url
+        config['api_key'] = api_key                                          #Not needed for llama-cpp TODO:get from ENV for locl models
+
     return Crew(
-        agents  =created_agents,
-        tasks   =created_tasks,
-        verbose =True,               #TODO remove hardcoding
-        process =Process.sequential, #TODO remove hardcoding
-        memory = True,               #TODO remove hardcoding 
-        embedder={                   #TODO remove hardcoding  
-			"provider": "azure_openai",#TODO make this configurable
-			"config":{
-				"model": 'text-embedding-ada-002',#TODO make this configurable
-				"deployment_name": "text-embedding-ada-002",#TODO make this configurable and optional
-			}
+        agents  = created_agents,
+        tasks   = created_tasks,
+        verbose = verbose,
+        process = process,
+        memory  = memory,              
+        embedder= {                   
+			"provider": provider, 
+			"config": config
         }
     )
 
@@ -115,7 +201,7 @@ if __name__ == "__main__":
         helpers.greetings_print()                                          #shows google sheets template file.
         sheet_url = input("Please provide the URL of your google sheet:")
     
-    agents_df, tasks_df = Sheets.parse_table(sheet_url)
+    agents_df, tasks_df, crew_df, models_df = Sheets.parse_table(sheet_url)
     helpers.after_read_sheet_print(agents_df, tasks_df)                     #Print overview of agents and tasks
     
     console = Console()
@@ -123,21 +209,22 @@ if __name__ == "__main__":
    
 
     with Progress() as progress:
+        #Create Agents
         agent_task = progress.add_task("[cyan]Creating agents......", total=len(agents_df))   #Agent progress bar  
         llm_task = progress.add_task("[cyan]  Pulling llm model...", total=100)  # 100 is a placeholder    
-        agents_df['crewAIAgent'] = agents_df.apply(lambda row: create_agents_from_df(row, progress=progress, agent_task=agent_task, llm_task=llm_task), axis=1)
+        agents_df['crewAIAgent'] = agents_df.apply(lambda row: create_agents_from_df(row, models_df=models_df, progress=progress, agent_task=agent_task, llm_task=llm_task), axis=1)
         created_agents = agents_df['crewAIAgent'].tolist()
         
-        
+        #Create Tasks
         task_task = progress.add_task("[cyan]Creating tasks...", total=1)                   #Task progress bar
-        assignment = tasks_df['Assignment'][0]
+        assignment = crew_df['Assignment'][0]
         tasks_df['crewAITask'] = tasks_df.apply(lambda row: create_tasks_from_df(row, assignment, created_agents), axis=1)
         created_tasks = tasks_df['crewAITask'].tolist()
         progress.advance(task_task)
         
         # Creating crew
         crew_task = progress.add_task("[cyan]Creating crew...", total=1)                    #Crew progress bar
-        crew = create_crew(created_agents, created_tasks)
+        crew = create_crew(created_agents, created_tasks, crew_df)
         progress.advance(crew_task)
         
         progress.stop
