08b4b66d96a60dab4e619b0a7dc0100a02e2a75e
yuriwa <yuri.warczynski@gmail.com>
2024-04-30 10:02:36 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,69 +1,61 @@
-import  os
-import logging
-from   utils.helpers   import load_env
-logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-os.environ['ANONYMIZED_TELEMETRY']  = 'False'                       # Disable interpreter telemetry
-os.environ['EC_TELEMETRY']          = 'False'                       # Disable embedchain telemetry
-os.environ['HAYSTACK_TELEMETRY_ENABLED'] = "False"                  # Disable crewai telemetry
-load_env("../../ENV/.env", ["OPENAI_API_KEY",])                     # Load API keys from ENV #Gives nice error if listed ENV variables are not set
 
-from textwrap           import dedent
-from crewai             import Crew, Task, Agent, Process
-
-
-#from langchain_community.llms import OpenAI
-from langchain_community.llms import Ollama
-from langchain_community.llms import LlamaCpp
-from langchain_openai import ChatOpenAI, AzureChatOpenAI
-from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
-from utils.helpers   import get_llm
-
-#crewai-sheets-ui
-from tools.tools        import ToolsMapping
-from utils              import Sheets
-from utils              import helpers
-from rich.console       import Console
-from rich.progress      import Progress
-from rich.table         import Table
-from rich.console       import Console
-
-import argparse
-import signal
-import sys
-import pandas as pd
-
-# Define a function to handle termination signals
-def signal_handler(sig, frame):
-    print("\n\nReceived termination signal. Shutting down gracefully...\n\n")
-    # Perform cleanup actions here
-    # Close connections, release resources, etc.
+import      logging
+from        rich.logging    import RichHandler
+from        rich.console    import Console
+###
+console   = Console()
+logging.basicConfig(level="ERROR", format="%(message)s", datefmt="[%X]", handlers=[RichHandler(console=console, rich_tracebacks=True)])
+logger  = logging.getLogger("rich")
+###
+import  os, sys,signal
+###
+os.environ['HAYSTACK_TELEMETRY_ENABLED'] = 'False'                       #Attmpt to turn off telemetry
+os.environ['ANONYMIZED_TELEMETRY']       = 'False'                       # Disable interpreter telemetry
+os.environ['EC_TELEMETRY']               = 'False'                       # Disable embedchain telemetry
+###
+def signal_handler(sig, frame): 
+    print("\n\nI received a termination signal. You are the Terminator?! I'll shut down gracefully...\n\n")
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
+###
+from rich.progress  import Progress, BarColumn, TextColumn, TimeRemainingColumn
+from rich.table     import Table
+from textwrap       import dedent
+from crewai         import Crew, Task, Agent, Process
 
-# Helper function to create agents
-def create_agents_from_df(row, 
-                          models_df = None, 
-                          progress  = None,  #rich pb
-                          agent_task= None,  #rich pb
-                          llm_task  = None): #rich pb
+from langchain_community.llms import Ollama
+from langchain_community.llms import LlamaCpp
+from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
+from langchain_openai       import ChatOpenAI, AzureChatOpenAI
+
+from utils.agent_crew_llm import get_llm
+from utils.tools_mapping import ToolsMapping
+from utils.cli_parser import get_parser
+from utils.helpers import load_env, is_valid_google_sheets_url, get_sheet_url_from_user
+from utils     import Sheets, helpers
+
+import pandas as pd
+
+def create_agents_from_df(row, models_df = None,tools_df  = None): 
     def get_agent_tools(tools_string):
         tool_names = [tool.strip() for tool in tools_string.split(',')]
-        return [getattr(ToolsMapping, tool) for tool in tool_names if hasattr(ToolsMapping, tool)]
+        tools_mapping = ToolsMapping(tools_df, models_df)  # Get the ToolsMapping instance
+        tools_dict = tools_mapping.get_tools()  # Get the dictionary of tools from the instance
+        return [tools_dict[tool] for tool in tool_names if tool in tools_dict]
     
-    role             = row['Agent Role']
-    goal             = dedent(row['Goal'])
-    backstory        = dedent(row['Backstory'])
-    allow_delegation = helpers.str_to_bool(row['Allow delegation'])
-    verbose          = helpers.str_to_bool(row['Verbose'])
-    tools            = get_agent_tools(row['Tools'])
-    memory           = helpers.str_to_bool(row['Memory'])
-    max_iter         = row['Max_iter']
-    
-    #region LLMs
-    model_name       = row.get('Model Name', 'gpt-4-turbo-preview').strip()
-    temperature      = float(row.get('Temperature', 0.7))
-    
+    role             = row.get('Agent Role',    "Assistant")
+    goal             = row.get('Goal',          "To assist the human in their tasks")
+    model_name       = row.get('Model Name',    'gpt-4-turbo-preview').strip()
+    backstory        = row.get('Backstory',     "...")
+    temperature      = row.get('Temperature',   0.8)
+    max_iter         = row.get('Max_iter' ,     15)
+    verbose          = row.get('Verbose',       True)
+    memory           = row.get('Memory',        True)
+    tools_string     = row.get('Tools')
+    tools            = get_agent_tools(tools_string)
+    allow_delegation = row.get('Allow delegation', False)
+
     # Retrieve Agent model details
     model_details = models_df[models_df['Model'].str.strip() == model_name] # Filter the models dataframe for the specific model
 
@@ -76,7 +68,7 @@ def create_agents_from_df(row,
         provider    = str(model_details['Provider'].iloc[0]) if 'Provider' in model_details.columns and not pd.isna(model_details['Provider'].iloc[0]) else None
         base_url    = str(model_details['base_url'].iloc[0]) if 'base_url' in model_details.columns and not pd.isna(model_details['base_url'].iloc[0]) else None
         deployment  = str(model_details['Deployment'].iloc[0]) if 'Deployment' in model_details.columns and not pd.isna(model_details['Deployment'].iloc[0]) else None
-        
+
         llm = get_llm(
                     model_name  = model_name, 
                     temperature = temperature, 
@@ -84,10 +76,7 @@ def create_agents_from_df(row,
                     provider    = provider,
                     base_url    = base_url,
                     deployment  = deployment, 
-                    progress    = progress, 
-                    llm_task    = llm_task
-                    
-                )
+                )        
     
     # Retrieve function calling model details
     function_calling_model_name = row.get('Function Calling Model', model_name)
@@ -113,10 +102,8 @@ def create_agents_from_df(row,
                     provider    = provider,
                     base_url     = base_url,
                     deployment  = deployment,
-                    progress    = progress,
-                    llm_task     = llm_task
                 )
-    #endregion
+
     
     agent_config = {
         #agent_executor:                                            #An instance of the CrewAgentExecutor class.
@@ -133,13 +120,12 @@ def create_agents_from_df(row,
         #step_callback:                                             #Callback to be executed after each step of the agent execution.
         #callbacks:                                                 #A list of callback functions from the langchain library that are triggered during the agent's execution process
     }                                                               
-    progress.update(agent_task, advance=1)
     return Agent(config = agent_config)      
         
 def get_agent_by_role(agents, desired_role):
     return next((agent for agent in agents if agent.role == desired_role), None)
             
-def create_tasks_from_df(row, assignment, created_agents):
+def create_tasks_from_df(row, assignment, created_agents, **kwargs):
     description     = row['Instructions'].replace('{assignment}', assignment)
     desired_role    = row['Agent']
 
@@ -151,7 +137,7 @@ def create_tasks_from_df(row, assignment, created_agents):
 
 def create_crew(created_agents, created_tasks, crew_df):
     #Embedding model (Memory)
-    memory = helpers.str_to_bool(crew_df['Memory'][0]) 
+    memory = crew_df['Memory'][0] 
     embedding_model = crew_df['Embedding model'][0]
     deployment_name = models_df.loc[models_df['Model'] == embedding_model, 'Deployment'].values[0]
     provider = models_df.loc[models_df['Model'] == embedding_model, 'Provider'].values[0]
@@ -168,7 +154,7 @@ def create_crew(created_agents, created_tasks, crew_df):
         os.environ["OPENAI_API_KEY"] = os.environ["AZURE_OPENAI_KEY"]
 
     if provider == 'openai':
-        embedder_config['api_key'] = os.environ.get("OPENAI_API_KEY")
+        embedder_config['api_key'] = os.environ.get("SECRET_OPENAI_API_KEY")
         os.environ["OPENAI_BASE_URL"] = "https://api.openai.com/v1"
 
     else: #Any other openai compatible e.g. ollama or llama-cpp				
@@ -176,6 +162,8 @@ def create_crew(created_agents, created_tasks, crew_df):
         api_key = 'NA'
         embedder_config['base_url'] = base_url
         embedder_config['api_key'] = api_key
+
+
     
     #Groq doesn't have an embedder
     
@@ -197,7 +185,7 @@ def create_crew(created_agents, created_tasks, crew_df):
             deployment  = manager_deployment
         )                                 
 
-    verbose = helpers.str_to_bool(crew_df['Verbose'][0])
+    verbose = crew_df['Verbose'][0]
     process = Process.hierarchical if crew_df['Process'][0] == 'hierarchical' else Process.sequential
     
     return Crew(
@@ -214,141 +202,51 @@ def create_crew(created_agents, created_tasks, crew_df):
     )
 
 if __name__ == "__main__":
+    helpers.greetings_print()
+    args        = get_parser()
+    log_level   = args.loglevel.upper() if hasattr(args, 'loglevel') else "ERROR"
+    logger.setLevel(log_level)
 
-    #Parse comman line arguments
-    parser  = argparse.ArgumentParser()
-    parser.add_argument('--sheet_url', help='The URL of the google sheet')
-    args    = parser.parse_args()
-    
-    if args.sheet_url:
+    load_env("../../ENV/.env", ["OPENAI_API_KEY",])  
+
+    if hasattr(args, "sheet_url") and args.sheet_url and is_valid_google_sheets_url(args.sheet_url):
         sheet_url = args.sheet_url
     else:
-        helpers.greetings_print()                                          #shows google sheets template file.
-        sheet_url = input("Please provide the URL of your google sheet:")
-    
-    agents_df, tasks_df, crew_df, models_df = Sheets.parse_table(sheet_url)
-    helpers.after_read_sheet_print(agents_df, tasks_df)                     #Print overview of agents and tasks
-    
-    console = Console()
-    progress = Progress(transient=True)
-   
+        sheet_url = get_sheet_url_from_user()
 
-    with Progress() as progress:
-        #Create Agents
-        agent_task = progress.add_task("[cyan]Creating agents......", total=len(agents_df))   #Agent progress bar  
-        llm_task = progress.add_task("[cyan]  Pulling llm model...", total=100)  # 100 is a placeholder    
-        agents_df['crewAIAgent'] = agents_df.apply(lambda row: create_agents_from_df(row, models_df=models_df, progress=progress, agent_task=agent_task, llm_task=llm_task), axis=1)
-        created_agents = agents_df['crewAIAgent'].tolist()
-        
-        #Create Tasks
-        task_task = progress.add_task("[cyan]Creating tasks...", total=1)                   #Task progress bar
-        assignment = crew_df['Assignment'][0]
-        tasks_df['crewAITask'] = tasks_df.apply(lambda row: create_tasks_from_df(row, assignment, created_agents), axis=1)
-        created_tasks = tasks_df['crewAITask'].tolist()
-        progress.advance(task_task)
-        
-        # Creating crew
-        crew_task = progress.add_task("[cyan]Creating crew...", total=1)                    #Crew progress bar
-        crew = create_crew(created_agents, created_tasks, crew_df)
-        progress.advance(crew_task)
-        
-        progress.stop
-        console.print("[green]Crew created successfully!")
-
-    results = crew.kickoff()
-
+    # Define a function to handle termination signals
     terminal_width = console.width
-
-   # Ensure the terminal width is at least 120 characters
     terminal_width = max(terminal_width, 120)
 
+    #Enter main process
+    agents_df, tasks_df, crew_df, models_df, tools_df = Sheets.parse_table(sheet_url)
+    helpers.after_read_sheet_print(agents_df, tasks_df)                     #Print overview of agents and tasks
+    
+
+
+    #Create Agents
+    agents_df['crewAIAgent'] = agents_df.apply(lambda row: create_agents_from_df(row, models_df=models_df, tools_df=tools_df), axis=1)
+    created_agents = agents_df['crewAIAgent'].tolist()
+            
+    #Create Tasks
+    assignment = crew_df['Assignment'][0]
+    tasks_df['crewAITask']  = tasks_df.apply(lambda row: create_tasks_from_df(row, assignment, created_agents), axis=1)
+    created_tasks = tasks_df['crewAITask'].tolist()
+                
+    # Creating crew
+    console.print("[green]I've created the crew for you. Let's start working on these tasks! :rocket: [/green]")
+    crew = create_crew(created_agents, created_tasks, crew_df) 
+
+    try:
+        results = crew.kickoff()
+    except Exception as e:
+        console.print(f"[red]I'm sorry, I couldn't complete the tasks :( Here's the error I encountered: {e}")
+        sys.exit(0)
+    
     # Create a table for results
     result_table = Table(show_header=True, header_style="bold magenta")
-    result_table.add_column("Here are the results", style="green", width=terminal_width)
+    result_table.add_column("Here are the results, see you soon =) ", style="green", width=terminal_width)
 
-    
     result_table.add_row(str(results))
-
     console.print(result_table)
     console.print("[bold green]\n\n")
-
-
- # #llm =ChatOpenAI(    
-    #     #base_url       = "http://localhost:1234/v1",
-    #     #api_key        = "lm-studio",
-    #     #callback_manager= callback_manager,
-    #     #max_tokens     = -1,
-    #     #streaming      = True,
-    #     #verbose        = True, 
-    #     #Not available in OpenAI: n_predict = -1, top_k = 40, repeat_penalty= 1.1, min_p= 0.05
-    #     model_name     = "gpt-4-turbo-preview",
-    #     temperature    = 0.9,
-    #     #model_kwargs={
-    #                     #"n_predict":-1,
-    #                     #"top_k":40,
-    #                     #"repeat_penalty":1.24,
-    #                     #"min_p":0.05,
-    #                     #"top_p":0.95,
-    #                     #}
-    #     )
-
-
-
- 
-       #callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])  # Callbacks support token-wise streaming
-
-        #Agent LLM fields
-        #model_name     = row.get('Model Name', 'gpt-4-turbo-preview') #Selection of model for OpenAI. Or HuggingFace Model name for caching (via llamacpp)
-        #temperature    = float(row.get('Temperature', 0.0))
-        #base_url       = row.get('Base URL', None)                    #For "local" OpenAI compatible LLM  
-        #chat_format    = "vicuna"                                     # or "llama-2", vicuna,  etc.., for llama-cpp-python //TODO remove hardcoding. 
-        #llm_params = {'model_name': model_name, 'temperature': temperature}
-        #Adjust the instantiation based on whether a base URL is provided
-        #if base_url is not None and base_url == base_url:  # if base_url is not NaN...
-        #       llm_params['base_url'] = base_url
-        #       max_tokens = 2048 #//TODO Remove hardcoding
-        #llm_params['max_tokens'] = max_tokens
-        
-    #Function calling LLM fields
-    #FOR LLAMACPP CONFIG
-    #llm = LlamaCpp(
-    #   #model_path="./../llama.cpp/models/Hermes-2-Pro-Mistral-7B-GGUF/Hermes-2-Pro-Mistral-7B.Q8_0.gguf",
-    #   #model_path="./../../../.cache/lm-studio/models/mradermacher/Nous-Capybara-limarpv3-34B-i1-GGUF/Nous-Capybara-limarpv3-34B.i1-Q4_K_M.gguf",
-    #   callback_manager    = callback_manager,
-    #   verbose             = True,  # Verbose is required to pass to the callback manager
-    #   streaming           = True, 
-    #   n_gpu_layers        = -1,
-    #   n_threads           = 4,
-    #   f16_kv              = True,  # MUST set to True, otherwise you will run into problem after a couple of calls
-    #   n_ctx               = 36000, #defined by model
-    #   n_batch             = 2048,
-    #   max_new_tokens      = 512,
-    #   max_length          = 4096,
-    #   last_n_tokens_size  = 1024,
-    #   temperature         = 0.0, 
-    #   chat_template       = "[INST] <<SYS>>\n{system}\n<</SYS>>\n\n{instruction} [/INST]",
-    #                         #{System}\nUSER: {user}\nASSISTANT: {response}</s>",
-    #   chat_format         = "llama-2",
-    #   max_tokens          = 256, 
-    #   top_p               = 0.5,
-    #   top_k               = 10,
-    #   use_mlock           = True,
-    #   repeat_penalty      = 1.5,
-    #   seed                = -1,
-    #   model_kwargs        = {"model_name":"01-ai/Yi-34B", "offload_kqv":True, "min_p":0.05},
-    #   stop                = ["\nObservation"],     
-    # )
-        # print("\nCreating agents:.\n")
-    # agents_df['crewAIAgent'] = agents_df.apply(create_agents_from_df, axis=1)
-    # created_agents = agents_df['crewAIAgent'].tolist()
-
-    # print("Creating tasks:..\n")
-    # assignment = tasks_df['Assignment'][0]
-    # tasks_df['crewAITask'] = tasks_df.apply(lambda row: create_tasks_from_df(row, assignment, created_agents), axis=1)
-    # created_tasks = tasks_df['crewAITask'].tolist()
-    
-    # print("Creating crew:...\n")
-    # crew = create_crew(created_agents, created_tasks)
-    # results = crew.kickoff()
-
-    # Print results
